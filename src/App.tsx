cbcd927035552c92b63d4400bcc9c8f0d1e50855
Abraham Vidal <antonio.vidal95@hotmail.com>
2022-04-13 19:32:49 -0400

    add new submodule

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Checkbox, Textfield } from "./submodule-app/src/components";
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button />
+      <Checkbox />
+      <Textfield />
     </div>
   );
 }
